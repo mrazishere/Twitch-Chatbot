c155942608a22a254a23ca1ccbec1f43340d4308
MrAZ <mrazishere@gmail.com>
2021-12-14 00:54:23 +0800

    add searcj feature for !dad

--- app.js
+++ app.js
@@ -210,24 +210,47 @@ client.on('message', (channel, tags, message, self) => {
     // Dad Jokes Function
     async function dad(){
         input = message.slice(5);
-        const fetchResponse = await fetch('https://icanhazdadjoke.com/', {method: 'GET', headers: {'accept': 'text/plain', 'content-type': 'text/plain'}})
-        .then(response => {
-            if (response.ok) {
-                response.text().then((data) => {
-                    sleep(1000);
-                    if(input === "") {
+        if(input === "") {
+            const fetchResponse = await fetch('https://icanhazdadjoke.com/', {method: 'GET', headers: {'accept': 'text/plain', 'content-type': 'text/plain'}})
+            .then(response => {
+                if (response.ok) {
+                    response.text().then((data) => {
+                        sleep(1000);
                         console.log(data);
                         client.say(channel, data);
-                    }
-                    //console.log(data);
-                });
-            } else {
-                client.say(channel, "Sorry, API is unavailable right now. Please try again later.");
-            }
-        }).
-        catch(error => {
-              console.log(error);
-        });
+                        //console.log(data);
+                    });
+                } else {
+                    client.say(channel, "Sorry, API is unavailable right now. Please try again later.");
+                }
+            }).
+            catch(error => {
+                  console.log(error);
+            });
+        } else {
+            const fetchResponse = await fetch('https://icanhazdadjoke.com/search?term=' + input, {method: 'GET', headers: {'accept': 'application/json', 'content-type': 'application/json'}})
+            .then(response => {
+                if (response.ok) {
+                    response.json().then((data) => {
+                        var outputArr = JSON.parse(JSON.stringify(data));
+                        sleep(1000);
+                        if (outputArr['total_jokes'] == 0) {
+                            console.log("No Jokes found");
+                            client.say(channel, "Sorry, nothing found with the search term: " + input);
+                        } else {
+                            var random = Math.floor(Math.random() * outputArr['results'].length);
+                            console.log(outputArr['results'][random]['joke']);
+                            client.say(channel, outputArr['results'][random]['joke']);
+                        }
+                    });
+                } else {
+                    client.say(channel, "Sorry, API is unavailable right now. Please try again later.");
+                }
+            }).
+            catch(error => {
+                  console.log(error);
+            });
+        }
         return;
     }
 
@@ -364,6 +387,10 @@ client.on('message', (channel, tags, message, self) => {
             return;
         }
     }
+
+    if (message.includes("!daddy")) {
+        return;
+    }   
 
     if (message.includes("!forex")) {
         forex();
