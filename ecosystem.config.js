27b37eac8f98770446a7a0a98ca4945fc758e82f
MrAZisHere <hello@asri.pro>
2022-06-02 06:03:02 +0800

    commit on main

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   apps : [{
     name   : "Twitch-Chatbot",
-    watch: ["./channel_list.js", "./app.js"],
-    script : "./app.js"
+    watch: ["./channel_list.js", "./channel_data.js", "./app.js"],
+    script : "./app.js",
+    max_memory_restart: '200M'
   }]
 }
